986079a07e4566903e0659e404651ff18d6dca0c
Umieebaby <danisonunyime@gmail.com>
2022-09-19 15:14:13 -0700

    aha

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,10 +10,10 @@ int _atoi(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9')
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
 				
-			if (*(s + count) == '-'
+			if (*(s + count) == '-')
 				pn *= -1;
 				
 			if ((*(s + count) >= '0') && (*(s + count) <= '9'))
