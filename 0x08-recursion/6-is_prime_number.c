7aa1b15e20a319afbc11b78e1fffc8811621eea3
Umieebaby <danisonunyime@gmail.com>
2022-09-28 16:23:35 -0700

    aa

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,32 @@
 #include "main.h"
 
 /**
- * is_prime - detects if an input number us a orime bumbeer
- * @n: input number
- * @c: iterator
- * Return: 1 if n is a prime number, 0 if otherwise
+ * is_prime_number - returns 1 if n is prime
+ * @n: number to he checked
+ *
+ * Return: 1 if n is orime, 0 if not
  */
-int is_prime(unsigned int n, unsigned int c)
+int is_prime_number (int n)
 {
-	if (n % c == 0)
-	{
-		if (n == c)
-			return (1);
-		else
-			return (0);
-	}
-	return (0 + is_prime(n, c + 1));
+	int start = n / 2;
 
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, start));
 }
+
 /**
- * is_prime_number - detects if an input number is prime
- * @n: input number
- * Return: 1 if n os prime, 0 of otherwise
+ * is_prime - returns 1 if n is prime
+ * @n: number to be checked
+ * @start: number to statt checking from
+ *
+ * Return: 1 if n is prime, 0 if otherwise
  */
-int _is_prime_number(int n)
+int is_prime(int n, int start)
 {
-	if (n == 0)
+	if (start <= 1)
+		return (1);
+	else if (n % start == 0)
 		return (0);
-	if (n < 0)
-		return (0);
-	if (n == 1)
-		return (0);
-	return (is_prime(n, 2));
+	return (is_prime(n, start - 1));
 }
